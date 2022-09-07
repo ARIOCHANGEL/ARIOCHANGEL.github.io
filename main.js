24165b7474e96fc2db1a1c50745941aa14a8bd9c
ARIOCHANGEL <113054666+ARIOCHANGEL@users.noreply.github.com>
2022-09-07 18:44:35 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-let burger = document.querySelector('#burger');
-let menu = document.querySelector('.menu');
-
-burger.addEventListener('click', () => {
-				menu.classList.toggle('abierto');
-				burger.classList.toggle('abierto');
-});
-
